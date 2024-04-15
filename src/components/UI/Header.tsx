ef27744dcd7ff7bd3f442a8c9d2bf56579b56c1e
Thiago Leite <thiagofmleite@gmail.com>
2024-04-15 10:53:20 -0300

    task: Sidenav component

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { useDataContext } from "@/context/DataContext";
 import DateRange from "../Form/DateRange";
 import Months from "../Form/Months";
 
 const Header = () => {
-  const { data } = useDataContext();
+  const [title, setTitle] = React.useState("Resumo");
+
   return (
     <header className="mb">
-      <div className="mb">
+      <div className="dateRange mb">
         <DateRange />
+        <h1 className="box bg-3">{title}</h1>
       </div>
       <Months />
     </header>
--- src/components/UI/Sidenav.tsx
+++ src/components/UI/Sidenav.tsx
@@ -1,7 +1,56 @@
 import React from "react";
+import FintechSvg from "@assets/FintechSvg";
+import resumo from "@assets/icons/resumo.svg";
+import vendas from "@assets/icons/vendas.svg";
+import webhooks from "@assets/icons/webhooks.svg";
+import configuracoes from "@assets/icons/configuracoes.svg";
+import contato from "@assets/icons/contato.svg";
+import sair from "@assets/icons/sair.svg";
 
 const Sidenav = () => {
-  return <div>Sidenav</div>;
+  return (
+    <nav className="sidenav box bg-3">
+      <FintechSvg title="Fintech Logo" />
+      <ul>
+        <li>
+          <span>
+            <img src={resumo} alt="" />
+          </span>
+          <a href="">Resumo</a>
+        </li>
+        <li>
+          <span>
+            <img src={vendas} alt="" />
+          </span>
+          <a href="">Vendas</a>
+        </li>
+        <li>
+          <span>
+            <img src={webhooks} alt="" />
+          </span>
+          <a>Webhooks</a>
+        </li>
+        <li>
+          <span>
+            <img src={configuracoes} alt="" />
+          </span>
+          <a>Configurações</a>
+        </li>
+        <li>
+          <span>
+            <img src={contato} alt="" />
+          </span>
+          <a>Contato</a>
+        </li>
+        <li>
+          <span>
+            <img src={sair} alt="" />
+          </span>
+          <a>Sair</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Sidenav;
