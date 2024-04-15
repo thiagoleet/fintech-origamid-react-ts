0c0404617408f52ed7168acc702504a37aa60311
Thiago Leite <thiagofmleite@gmail.com>
2024-04-15 09:35:15 -0300

    task: Using CSS with Typescript

--- src/components/Form/DateInput.tsx
+++ src/components/Form/DateInput.tsx
@@ -4,11 +4,40 @@ type IDateInput = React.ComponentProps<"input"> & {
   label: string;
 };
 
+const generalStyle: React.CSSProperties = {
+  color: "var(--color-2)",
+  padding: "var(--gap-s) 0.75rem",
+  backgroundColor: "var(--color-4)",
+  borderRadius: "var(--gap)",
+};
+
+const labelStyle: React.CSSProperties = {
+  display: "block",
+  marginBottom: "var(--gap-s)",
+  fontWeight: "bold",
+  ...generalStyle,
+};
+
+const inputStyle: React.CSSProperties = {
+  border: "none",
+  fontFamily: "monospace",
+  ...generalStyle,
+};
+
 const DateInput: React.FC<IDateInput> = ({ label, ...props }) => {
   return (
     <div>
-      <label htmlFor={props.id}>{label}</label>
-      <input type="date" id={props.id} name={props.id} {...props} />
+      <label htmlFor={props.id} style={labelStyle}>
+        {" "}
+        {label}
+      </label>
+      <input
+        type="date"
+        id={props.id}
+        name={props.id}
+        {...props}
+        style={inputStyle}
+      />
     </div>
   );
 };
