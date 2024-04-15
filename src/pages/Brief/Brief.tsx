597a49493c5b5cafd5f1a7fd5219ee46e6195bc3
Thiago Leite <thiagofmleite@gmail.com>
2024-04-15 10:18:37 -0300

    fix: show brief data

--- src/pages/Brief/Brief.tsx
+++ src/pages/Brief/Brief.tsx
@@ -1,9 +1,46 @@
 import { useDataContext } from "@/context/DataContext";
-import React from "react";
 
 const Brief = () => {
   const { data } = useDataContext();
-  return <div>Resumo</div>;
+
+  if (data === null) return null;
+
+  return (
+    <section>
+      <div className="resumo flex mb">
+        <div className="box">
+          <h2>Vendas</h2>
+          <span>
+            {data
+              .filter((i) => i.status !== "falha")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+
+        <div className="box">
+          <h2>Recebido</h2>
+          <span>
+            {data
+              .filter((i) => i.status === "pago")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+
+        <div className="box">
+          <h2>Processando</h2>
+          <span>
+            {data
+              .filter((i) => i.status === "procesando")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+      </div>
+      <div className="box">Gráficos</div>
+    </section>
+  );
 };
 
 export default Brief;
